a86eb3d78a1dcad05a3ff97b21208b44b399ccff
Danish <mailtodani007@gmail.com>
2017-04-27 01:42:04 +0530

    Website Snippets

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+  'name':'Website Snippets',
+  'description': 'This is snippet module. Through this you can get the designed snippets',
+  'version':'1.0',
+  'author':'Danish & Justin',
+
+  'data': [
+  'views/assets.xml',
+  'views/profile_card.xml'
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
